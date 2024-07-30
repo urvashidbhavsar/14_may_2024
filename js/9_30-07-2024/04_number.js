0068c2fe9b6432af9668e98b9ee6123384afe7bf
urvashi <urvashi.d.bhavsar@gmail.com>
2024-07-30 15:43:06 +0530

    30th changes

---
+++ js/9_30-07-2024/04_number.js
@@ -0,0 +1,17 @@
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+var num1 = 45.55, num2 = 45;
+console.log(Number.isFinite(num1));
+console.log(Number.isInteger(num1));
+console.log(Number.parseFloat(num1));
+console.log(Number.parseFloat(num2));
+
+console.log(Number.parseInt(num1));
+console.log(Number.parseInt(num2));
+
+console.log(num1.toString());
+
+console.log(num1.toExponential(2));
+console.log(num1.toFixed(5));
+console.log(num1.toPrecision(5));
---
+++ js/9_30-07-2024/06_string.js
@@ -0,0 +1,17 @@
+var str = "Hello User";
+
+console.log(str.charAt(2));
+console.log(str.charCodeAt(2));
+console.log(str.concat("!!!!!!!!!"));
+console.log(str.indexOf("User"));
+console.log(str.lastIndexOf("l"));
+console.log(str.search("User"));
+console.log(str.match("User"));
+console.log(str.replace("User", "World"));
+console.log(str.substr(1, 6));
+console.log(str.substring(1, 6));
+console.log(str.slice(1, 6));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+var stat = "       Hello           "
+console.log(stat.trim());
