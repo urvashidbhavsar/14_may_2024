37c1c65f1ae5b59c78f4f0ffb66ec12aa0641327
urvashi <urvashi.d.bhavsar@gmail.com>
2024-10-10 09:28:54 +0530

    rewind

--- AdvancedJs/01_08-10-2024/02_call_apply.js
+++ AdvancedJs/01_08-10-2024/02_call_apply.js
@@ -5,8 +5,14 @@ var person = {
 var per_details = {
     details: function () {
         return this.first + " " + this.last
+    },
+    city: function (mycity) {
+        return this.first + " " + this.last + " " + mycity
     }
 }
+
 console.log(per_details.details.call(person));
 console.log(per_details.details.apply(person));
 
+console.log(per_details.city.call(person, "Surat"));
+console.log(per_details.city.apply(person, ["Surat"]));
