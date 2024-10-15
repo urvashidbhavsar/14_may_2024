2db071fcc8b13fc17684255dd1b7fff3420d4a56
urvashi <urvashi.d.bhavsar@gmail.com>
2024-10-15 09:04:13 +0530

    15th changes

---
+++ AdvancedJs/02_15-10-2024/04_include.js
@@ -0,0 +1,16 @@
+var sen = "this is a js language";
+var word = "JS";
+
+var res = sen.includes(word) ? "true" : "false"
+console.log(res);
+
+console.log("=========================");
+// startwith and endwith 
+console.log(sen.startsWith("this"));
+console.log(sen.startsWith("T"));
+
+console.log(sen.endsWith("language"));
+console.log(sen.endsWith("e"));
+
+
+
