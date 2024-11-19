95ca821a7b93a615da122171a184e1872e87f8f1
urvashi <urvashi.d.bhavsar@gmail.com>
2024-11-19 09:04:41 +0530

    19th changes

--- AdvancedJs/09_14-11-2024/07_abEx2.js
+++ AdvancedJs/09_14-11-2024/07_abEx2.js
@@ -1,18 +1,18 @@
-class mymember {
-    constructor() {
-        this.empname;
-        throw new Error("You cant create instance of an abstract class");
-    }
-    display() {
-        return "Employee name : " + this.empname;
-    }
+function mymember() {
+    this.empname = "Abc";
+    throw new Error("You cant create instance of an abstract class");
 }
-class newmember {
-    constructor(empname) {
-        this.empname = empname;
-    }
+mymember.prototype.display = function () {
+    return "Employee name : " + this.empname;
+}
+function newmember(empname) {
+    this.empname = empname;
 }
 // var emp1 = new employee("Urvashi");
-newmember.prototype = Object.create(mymember.prototype)
+newmember.prototype = Object.create(mymember.prototype);
 var emp1 = new newmember("Urvashi");
 console.log(emp1);
+console.log(emp1.display());
+
+
+
